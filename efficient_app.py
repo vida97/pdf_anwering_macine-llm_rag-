b87471c163afa792de4dbe2c1bf0d4b8d127fae0
Vibhu Dagar <35463829+vida97@users.noreply.github.com>
2024-01-04 22:00:16 -0600

    Add files via upload

--- efficient_app.py
+++ efficient_app.py
@@ -10,30 +10,36 @@ from langchain import PromptTemplate
 import streamlit as st
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
-load_dotenv()
-nltk.download('punkt')
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
 
 st.title("MY PDF ANSWERING MACHINE")
 st.text("(Upload your PDF -- Ask a question)")
+
+load_dotenv()
+nltk.download('punkt')
+
+@st.cache_resource
+def load_model():
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
+
 target_dir = "pdfs"
 
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 def generate_embeddings(text):
-    # Use NLTK's sentence tokenizer
     sentences = nltk.tokenize.sent_tokenize(text)
+    model = load_model()
     embeddings = model.encode(sentences, convert_to_tensor=True)
     return embeddings, sentences
 
 def create_faiss_index(embeddings):
-    d = embeddings.shape[1]  # dimension of vectors
+    d = embeddings.shape[1]  
     index = faiss.IndexFlatL2(d)
-    index.add(embeddings.cpu().detach().numpy())  # add vectors to the index
+    index.add(embeddings.cpu().detach().numpy())  
     return index
 
 def search_in_index(index, query, sentences, top_k=5):
+    model = load_model()
     query_embedding = model.encode([query], convert_to_tensor=True)
     query_embedding_np = query_embedding.cpu().detach().numpy()
     distances, indices = index.search(query_embedding_np, top_k)
@@ -48,35 +54,9 @@ if uploaded_file:
         for page in pdf_reader.pages:
             extracted_text += page.extract_text()
 
-        # text_splitter = RecursiveCharacterTextSplitter(
-        #             # Set a really small chunk size, just to show.
-        #             chunk_size = 1024,
-        #             chunk_overlap  = 128,            
-        #         )
-
-        # chunks = text_splitter.create_documents([extracted_text])
-
         genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
         llm = ChatGoogleGenerativeAI(model="gemini-pro",
                              temperature=0.1)
-
-        # prompt_template = """
-        # Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-        # provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-        # Context:\n {context}?\n
-        # Question: \n{question}\n
-
-        # Answer:
-        # """
-        # prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-
-        # def create_chain(prompt):
-        #     model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
-        #     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-        #     return chain
-
-        # chain = create_chain(prompt)
-
 
         template = """
         As an expert in extracting relevant and meaningful information, your task is to process the input provided. The input contains a user's query followed by a text. They are separated by the phrase 'End of Query'. Carefully understand the query to extract the relevant information from the text.
@@ -96,25 +76,12 @@ if uploaded_file:
             chains=[question_chain]
         )
 
-        embeddings, sentences = generate_embeddings(extracted_text)
-
-        index = create_faiss_index(embeddings)
-
-        # text = st.text_input('Enter your question here ')
-        # if text:
-        #     st.write("Response : ")
-        #     with st.spinner("Searching for answers ..... "):
-        #         response = chain(
-        #             {"input_documents":chunks, "question": text},
-        #             return_only_outputs=True)
-        #         st.write(response['output_text'])
-        #     st.write("")
-
-
         text = st.text_input('Enter your question here ')
         if text:
             st.write("Response : ")
             with st.spinner("Searching for answers ..... "):
+                embeddings, sentences = generate_embeddings(extracted_text)
+                index = create_faiss_index(embeddings)
                 results = search_in_index(index, text, sentences)
                 txt = ''
                 for sentence, score in results:
@@ -127,7 +94,3 @@ if uploaded_file:
 
     except Exception as e:
         st.write(e)
-    
-
-
-
